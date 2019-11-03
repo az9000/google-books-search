7616e643acff650c464d5068af6b00534c631c7e
Fawzi <alami42@live.com>
2019-11-03 12:00:50 -0600

    cleanup after removing socket io

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require("express");
-const http = require("http");
-const socketIO = require("socket.io");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 // our server instance
-const server = http.createServer(app);
 const PORT = process.env.PORT || 3001;
 
 // Define middleware here
@@ -21,21 +18,7 @@ app.use(routes);
 // Connect to the Mongo DB
 mongoose.connect(process.env.GOOGLE_BOOKS_DB_URI || "mongodb://localhost/googleBooks");
 
-const io = socketIO(server);
-io.on("connection", socket => {
-  console.log("user connected");
-
-  socket.on('book saved', (title) => {
-    console.log('Book title: ', title)
-    io.sockets.emit('book saved', title)
-  });
-
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-});
-
 // Start the API server
-server.listen(PORT, function() {
+app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
